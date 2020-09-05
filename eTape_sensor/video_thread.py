fd0255d2496c0090410abe712e25456306fdef85
sktrinh12 <trinh.kcs85@gmail.com>
2020-09-05 04:53:09 -0400

    separated terminate and record functions in class; changed init parameters and looped h264 directory to clean-up

--- eTape_sensor/video_thread.py
+++ eTape_sensor/video_thread.py
@@ -6,11 +6,11 @@ import subprocess
 import os
 
 #rec_vid_dir = '/home/pi/mount/hampc/tp_logs/recorded_videos/'
-rec_vid_dir = os.path.join('/home/pi/hamilton_web_app_v1.2/tip_novus/static/videos')
+rec_vid_dir = os.path.join('/home/pi/mount/hampc/tp_logs/recorded_videos')
 
 def validate_trigger_cmd(vid_status_dict):
     #check_trigger = vid_status['trigger'] in ['on', 'off']
-    check_time = vid_status_dict['time'].isdigit() and int(vid_status_dict['time']) > 0
+    check_time = vid_status_dict['time'].isdigit() and int(vid_status_dict['time']) < 15
     check_workflow = 'workflow' in vid_status_dict['wrkflow_name'].lower()
     return all([item == True for item in [check_time, check_workflow]])
 
@@ -37,33 +37,41 @@ class RecordVideoClass:
 
     '''simple class that will record a video using the picamera that will be invoked by the ThreadWithReturnValue; able to start and stop manually'''
     def __init__(self):
-        self._running = True
+        self._camera = picamera.PiCamera()
+        self._camera.resolution = (640, 480)
+        #self._camera = None
+        self._file_name = ''
+        self._file_path = ''
 
     def terminate(self):
-        self._running = False
+        try:
+            self._camera.stop_recording()
+        except picamera.PiCameraNotRecording:
+            pass
+        self._camera.close()
+        file_name = self._file_name.split('.')[0] + '.mp4'
+        new_file_path= os.path.join(rec_vid_dir, 'mp4_format', file_name)
+        subproc_cmd = f"MP4Box -add {self._file_path} {new_file_path}"
+        #convert from h264 -> mp4
+        try:
+            output = subprocess.check_output(subproc_cmd, stderr=subprocess.STDOUT, shell=True)
+            handle_logs('Subprocess for MP4Box convesion to {} was successful'.format(self._file_name ))
+        except subprocess.CalledProcessError as e:
+            err_msg= f'Failed to convert file {self._file_path} - cmd:{e.cmd}; output:{e.output}'
+            handle_logs(('error',err_msg))
+
+        # clean up h264 files
+        if os.path.exists(new_file_path):
+            parent_dir = os.path.dirname(self._file_path)
+            for hfile in os.listdir(parent_dir):
+                os.remove(os.path.join(parent_dir, hfile))
 
     def record_video(self, record_time, wrkflw):
-        if self._running and int(record_time) != 0:
-            with picamera.PiCamera() as camera:
-                camera.resolution = (640, 480)
-                record_time = int(record_time)
-                ts = datetime.strptime(get_time(), '%Y-%b-%d %H:%M:%S').strftime('%Y-%b-%d_%H-%M-%S')
-                print(ts)
-                file_name = f'{ts}_{wrkflw}.h264'
-                file_path = os.path.join(rec_vid_dir,'h264_format', file_name)
-                camera.start_recording(file_path, quality=10) #lower is better
-                camera.wait_recording(record_time) #in seconds
-                camera.stop_recording()
-                file_name = file_name.split('.')[0] + '.mp4'
-                #print(f'filename: {file_name}')
-                #print(f'filepath: {file_path}')
-                new_file_path= os.path.join(rec_vid_dir, 'mp4_format', file_name)
-                subproc_cmd = f"MP4Box -add {file_path} {new_file_path}"
-                #convert from h264 -> mp4
-                try:
-                    output = subprocess.check_output(subproc_cmd, stderr=subprocess.STDOUT, shell=True)
-                    handle_logs('Subprocess for MP4Box convesion to {} was successful (record time = {}s)'.format(file_name, record_time))
-                except subprocess.CalledProcessError as e:
-                    err_msg= f'Failed to convert file {file_path} - cmd:{e.cmd}; output:{e.output}'
-                    handle_logs(('error',err_msg))
-        return file_path
+        #if self._running and int(record_time) < 15:
+        record_time = int(record_time)
+        ts = datetime.strptime(get_time(), '%Y-%b-%d %H:%M:%S').strftime('%Y-%b-%d_%H-%M-%S')
+        self._file_name = f'{ts}_{wrkflw}.h264'
+        self._file_path = os.path.join(rec_vid_dir,'h264_format', self._file_name)
+        self._camera.start_recording(self._file_path, quality=22) # lower is better
+        self._camera.wait_recording(record_time) #in seconds
+        return self._file_path
